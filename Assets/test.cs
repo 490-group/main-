f121658db0ad30ede3fbea36bbf8460a889527d1
Oscar Cansino <ocansino99@gmail.com>
2021-11-01 14:34:31 -0700

    test 2

--- Assets/test.cs
+++ Assets/test.cs
@@ -8,7 +8,7 @@ public class test : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello World!");
-        Debug.Log("Hey What's up -Oscar");
+        Debug.Log("Oscar");
     }
 
     // Update is called once per frame
