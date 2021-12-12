012691737e0b5d279e8686bee3e87e5530d4220a
michaelbarseghian <michael.barseghian@gmail.com>
2021-12-12 15:22:11 -0800

    Updated stats framework

--- Assets/Scripts/playerStats.cs
+++ Assets/Scripts/playerStats.cs
@@ -1,5 +1,33 @@
-// health, attack, defense, agility/speed, luck
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
+public class playerStats : MonoBehaviour{
+
+public Stat health;
+public int currentHealth {get; private set; }
+public Stat attack;
+public Stat defense;
+public Stat agility;
+public Stat damage;
+
+void Awake()
+{
+    currentHealth = health;
+}
+public void TakeDamage(int damage)
+{
+    damage -= defense.GetValue();
+    damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+    currentHealth -= damage;
+
+    if(currentHealth <= 0)
+    {
+        Die();
+    }
+}
+public virtual void Die()
+{
+
+}
+
+}
