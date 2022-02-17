98ed053d9d60f9c45051a53c49a4a663361a45a0
ocansino <oscar.cansino.457@my.csun.edu>
2022-02-16 20:19:02 -0800

    Phase 1- working

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -1,53 +1,66 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomSpawner : MonoBehaviour
-{
-    public int openingDirection;
-    private RoomTemplates templates;
-    private int rand;
-    private bool spawned = false;
+public class RoomSpawner : MonoBehaviour {
+
+	public int openingDirection;
+	// 1 --> need bottom door
+	// 2 --> need top door
+	// 3 --> need left door
+	// 4 --> need right door
+
+
+	private RoomTemplates templates;
+	private int rand;
+	public bool spawned = false;
     public int roomCount = 0;
-    void Start(){
-        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
-        Invoke("Spawn", 0.1f);
-    }
+    public int levelSize = 5;
+	public float waitTime = 4f;
 
-    // Update is called once per frame
-    void Spawn(){
-        if (spawned == false){
-           if(openingDirection == 1){
-                rand = Random.Range(0, templates.bottomRooms.Length);
-                Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
-                roomCount++;
-            }
-            else if(openingDirection == 2){
-                rand = Random.Range(0, templates.topRooms.Length);
-                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
-                roomCount++;
-            }
-            else if(openingDirection == 3){
-                rand = Random.Range(0, templates.leftRooms.Length);
-                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
-                roomCount++;
-            }
-            else if(openingDirection == 4){
-                rand = Random.Range(0, templates.rightRooms.Length);
-                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
-                roomCount++;
-            }
-            spawned = true;
+	void Start(){
+		Destroy(gameObject, waitTime);
+		templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+		Invoke("Spawn", 0.1f);
+	}
+
+
+	void Spawn(){
+		if(spawned == false && roomCount <= levelSize){
             roomCount++;
-        }
-        
+			if(openingDirection == 1){
+				// Need to spawn a room with a BOTTOM door.
+				rand = Random.Range(0, templates.bottomRooms.Length);
+				Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+			    roomCount++;
+            } else if(openingDirection == 2){
+				// Need to spawn a room with a TOP door.
+				rand = Random.Range(0, templates.topRooms.Length);
+				Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+			     roomCount++;
+            } else if(openingDirection == 3){
+				// Need to spawn a room with a LEFT door.
+				rand = Random.Range(0, templates.leftRooms.Length);
+				Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+			     roomCount++;
+            } else if(openingDirection == 4){
+				// Need to spawn a room with a RIGHT door.
+				rand = Random.Range(0, templates.rightRooms.Length);
+				Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+			     roomCount++;
+            }
+            roomCount++;
+			spawned = true;
+		}
+	}
 
-
-
-    }
-    void OnTriggerEnter2D(Collider2D other){
-        if(other.CompareTag("SpawnPoint")){
-           Destroy(gameObject); 
-        }
-    }
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.CompareTag("SpawnPoint")){
+			if(other.GetComponent<RoomSpawner>().spawned == false && spawned == false){
+				Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
+				Destroy(gameObject);
+			} 
+			spawned = true;
+		}
+	}
 }
