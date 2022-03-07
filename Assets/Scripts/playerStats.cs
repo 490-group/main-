2763228126c708c89c6a0cfd0ef69eb63c8e000d
michaelbarseghian <michael.barseghian@gmail.com>
2022-03-06 21:07:25 -0800

    Merged Stats to Demo

--- Assets/Scripts/playerStats.cs
+++ Assets/Scripts/playerStats.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStats : CharacterStats
+public class playerStats : CharacterStats
 {
     // Start is called before the first frame update
     
